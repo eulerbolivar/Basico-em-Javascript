6169a93c04f30ee731915bacb68d307b125fb2db
Euler Oliveira <euleragenda21@gmail.com>
2023-02-12 18:15:36 -0300

    Prime Number

--- Algorithms/primeNumber.js
+++ Algorithms/primeNumber.js
@@ -3,7 +3,7 @@ function isPrime(n) {
         return "não é primo"
     }
 
-    for (let i = 2; i < n; i++){
+    for (let i = 2; i <= Math.sqrt(n); i++){
         if (n % i === 0){
             return "não é primo"
         }
